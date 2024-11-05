b4fcfe940c0d42d4b5fc2aa0344cd76125864348
Nishchith Shetty <inishchith@gmail.com>
2024-11-05 04:55:37 -0800

    E2E Application Test on Github Action (#46)
    * draft app test
    
    * try making an API call
    
    * detach the app run
    
    * more experiments
    
    * fail workflow test
    
    * run 100k assets test
    
    * continue on fail for stop services step
    
    * get workflow status and runtime
    
    * remove redundant temporal server run call
    
    * try with 1m
    
    * Revert "remove redundant temporal server run call"
    
    This reverts commit 11eeab2c79e88a0f6c1ecd42a84c0a63dcfcc639.
    
    * bump time for 1m
    
    * add comment on PR
    
    * comment out tests (failing) /w @SanilK2108
    
    * try commenting out conditions
    
    * fix: unit tests and app-test action
    
    * Update branch names
    
    * fix step names
    
    * fix typo
    
    * fix: add condition to comment only if event is PR

--- tests/unit/test_workflow.py
+++ tests/unit/test_workflow.py
@@ -33,38 +33,22 @@ async def test_extraction_workflow():
         },
     }
 
-    mock_setup_output_directory = MagicMock()
-    mock_setup_output_directory.return_value = None
-
     mock_fetch_databases = MagicMock()
-    mock_fetch_databases.return_value = {
-        "database": {"raw": 1, "transformed": 1, "errored": 0}
-    }
+    mock_fetch_databases.return_value = {"typename": "database", "chunk_count": 1}
 
     mock_fetch_schemas = MagicMock()
-    mock_fetch_schemas.return_value = {
-        "schema": {"raw": 1, "transformed": 1, "errored": 0}
-    }
+    mock_fetch_schemas.return_value = {"typename": "schema", "chunk_count": 1}
 
     mock_fetch_tables = MagicMock()
-    mock_fetch_tables.return_value = {
-        "table": {"raw": 1, "transformed": 1, "errored": 0}
-    }
+    mock_fetch_tables.return_value = {"typename": "table", "chunk_count": 1}
 
     mock_fetch_columns = MagicMock()
-    mock_fetch_columns.return_value = {
-        "column": {"raw": 1, "transformed": 1, "errored": 0}
-    }
+    mock_fetch_columns.return_value = {"typename": "column", "chunk_count": 1}
 
-    mock_push_results_to_object_store = MagicMock()
-    mock_push_results_to_object_store.return_value = None
-
-    mock_teardown_output_directory = MagicMock()
-    mock_teardown_output_directory.return_value = None
-
-    @activity.defn(name="setup_output_directory")
-    async def wrapped_mock_setup_output_directory(output_path: str):
-        mock_setup_output_directory(output_path)
+    mock_transform_data = MagicMock()
+    mock_transform_data.return_value = 1
+    mock_write_type_metadata = MagicMock()
+    mock_write_type_metadata.return_value = None
 
     @activity.defn(name="fetch_databases")
     async def wrapped_mock_fetch_databases(workflow_config: Dict[str, Any]):
@@ -82,15 +66,15 @@ async def test_extraction_workflow():
     async def wrapped_mock_fetch_columns(workflow_config: Dict[str, Any]):
         return mock_fetch_columns(workflow_config)
 
-    @activity.defn(name="push_results_to_object_store")
-    async def wrapped_mock_push_results_to_object_store(
-        output_config: Dict[str, str],
-    ):
-        mock_push_results_to_object_store(output_config)
+    @activity.defn(name="transform_data")
+    async def wrapped_mock_transform_data(workflow_args: Dict[str, Any]):
+        return mock_transform_data(workflow_args)
 
-    @activity.defn(name="teardown_output_directory")
-    async def wrapped_mock_teardown_output_directory(output_path: str):
-        mock_teardown_output_directory(output_path)
+    @activity.defn(name="write_type_metadata")
+    async def wrapped_mock_write_type_metadata(
+        workflow_args: Dict[str, Any],
+    ):
+        mock_write_type_metadata(workflow_args)
 
     try:
         env = await WorkflowEnvironment.start_local()
@@ -99,13 +83,12 @@ async def test_extraction_workflow():
             task_queue="test_queue",
             workflows=[PostgresWorkflowWorker],
             activities=[
-                wrapped_mock_setup_output_directory,
                 wrapped_mock_fetch_databases,
                 wrapped_mock_fetch_schemas,
                 wrapped_mock_fetch_tables,
                 wrapped_mock_fetch_columns,
-                wrapped_mock_push_results_to_object_store,
-                wrapped_mock_teardown_output_directory,
+                wrapped_mock_transform_data,
+                wrapped_mock_write_type_metadata,
             ],
             workflow_runner=SandboxedWorkflowRunner(
                 restrictions=SandboxRestrictions.default.with_passthrough_modules(
