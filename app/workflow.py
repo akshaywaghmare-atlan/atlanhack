604199e599d22e968e85c8b734001934c475c033
Nishchith Shetty <inishchith@gmail.com>
2025-01-23 13:14:32 -0800

    fix: support for correct view definition (#90)

--- app/workflow.py
+++ app/workflow.py
@@ -1,5 +1,5 @@
 import os
-from typing import cast
+from typing import Any, Dict, Union, cast
 from urllib.parse import quote_plus
 
 from application_sdk.workflows.controllers import (
@@ -19,6 +19,8 @@ from application_sdk.workflows.sql.resources.sql_resource import (
 )
 from application_sdk.workflows.sql.workflows.workflow import SQLWorkflow
 from application_sdk.workflows.transformers.atlas import AtlasTransformer
+from application_sdk.workflows.transformers.atlas.sql import Table
+from pyatlan.model import assets
 
 from app.const import (
     COLUMN_EXTRACTION_SQL,
@@ -57,12 +59,50 @@ class PostgresWorkflow(SQLWorkflow):
     sql_resource: SQLResource | None = PostgreSQLResource(SQLResourceConfig())
 
 
+class PostgresTable(Table):
+    @classmethod
+    def parse_obj(
+        cls, obj: Dict[str, Any]
+    ) -> Union[
+        assets.Table, assets.View, assets.MaterialisedView, assets.SnowflakeDynamicTable
+    ]:
+        """
+        Postgres view and materialized view definitions are select queries,
+        so we need to format the view definition to be a valid SQL query.
+
+        src: https://github.com/atlanhq/marketplace-packages/blob/master/packages/atlan/postgres/transformers/view.jinja2
+        """
+        if obj.get("table_type") == "VIEW":
+            view_definition = "CREATE OR REPLACE VIEW {view_name} AS {query}"
+            obj["view_definition"] = view_definition.format(
+                view_name=obj["table_name"], query=obj["view_definition"]
+            )
+        elif obj.get("table_type") == "MATERIALIZED VIEW":
+            view_definition = (
+                "CREATE OR REPLACE MATERIALIZED VIEW {view_name} AS {query}"
+            )
+            obj["view_definition"] = view_definition.format(
+                view_name=obj["table_name"], query=obj["view_definition"]
+            )
+
+        return super().parse_obj(obj)
+
+
+class CustomTransformer(AtlasTransformer):
+    def __init__(self, connector_name: str, tenant_id: str, **kwargs: Any):
+        super().__init__(connector_name, tenant_id, **kwargs)
+
+        self.entity_class_definitions["TABLE"] = PostgresTable
+        self.entity_class_definitions["VIEW"] = PostgresTable
+        self.entity_class_definitions["MATERIALIZED VIEW"] = PostgresTable
+
+
 class PostgresWorkflowBuilder(SQLWorkflowBuilder):
     preflight_check_controller: WorkflowPreflightCheckControllerInterface
 
     def __init__(self, application_name: str = APPLICATION_NAME):
         self.set_transformer(
-            AtlasTransformer(
+            CustomTransformer(
                 connector_name=application_name,
                 connector_type="postgres",
                 tenant_id=TENANT_ID,
