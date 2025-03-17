856932034c21055ec1e1724e9bb0d13dd69a9284
SanilK2108 <sanil.khurana@atlan.com>
2025-03-17 09:08:12 +0530

    debug: E2E tests

--- tests/e2e/test_postgres_wwi_workflow/test_postgres_wwi_workflow.py
+++ tests/e2e/test_postgres_wwi_workflow/test_postgres_wwi_workflow.py
@@ -3,5 +3,5 @@ import unittest
 from application_sdk.test_utils.e2e.base import BaseTest
 
 
-class TestPostgresWorkflow(unittest.TestCase, BaseTest):
+class TestPostgresWWIWorkflow(unittest.TestCase, BaseTest):
     pass
