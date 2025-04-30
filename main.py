3f7a3c9889b2c8fba01e581f3d6e0269acb3d846
Mustafa <100032108+TechyMT@users.noreply.github.com>
2025-04-30 23:37:50 +0530

    refactor: streamline SQL metadata extraction application setup (#236)
    * refactor: streamline SQL metadata extraction application setup
    
    * fix: update application-sdk dependency revision
    
    * fix: main.py
    
    * feat: application-sdk mapped to develop
    
    ---------
    
    Co-authored-by: inishchith <inishchith@gmail.com>

--- main.py
+++ main.py
@@ -1,56 +1,33 @@
 import asyncio
 
-from application_sdk.activities.metadata_extraction.sql import (
-    BaseSQLMetadataExtractionActivities,
+from application_sdk.application.metadata_extraction.sql import (
+    BaseSQLMetadataExtractionApplication,
 )
-from application_sdk.application.fastapi import Application, HttpWorkflowTrigger
-from application_sdk.clients.utils import get_workflow_client
-from application_sdk.handlers.sql import SQLHandler
-from application_sdk.worker import Worker
-from application_sdk.workflows.metadata_extraction.sql import (
-    BaseSQLMetadataExtractionWorkflow,
-)
+from application_sdk.constants import APPLICATION_NAME
 
 from app.clients import SQLClient
 from app.transformers.atlas import SQLAtlasTransformer
 
 
 async def main():
-    # setup workflow client for worker and application server
-    workflow_client = get_workflow_client()
-    await workflow_client.load()
-
-    # setup sql metadata extraction activities
-    # requires a sql client for source connectivity, handler for preflight checks, transformer for atlas mapping
-    activities = BaseSQLMetadataExtractionActivities(
-        sql_client_class=SQLClient,
-        handler_class=SQLHandler,
+    # Initialize the application
+    application = BaseSQLMetadataExtractionApplication(
+        name=APPLICATION_NAME,
+        client_class=SQLClient,
         transformer_class=SQLAtlasTransformer,
     )
 
-    # setup and start worker for workflow and activities execution
-    worker: Worker = Worker(
-        workflow_client=workflow_client,
-        workflow_classes=[BaseSQLMetadataExtractionWorkflow],
-        workflow_activities=BaseSQLMetadataExtractionWorkflow.get_activities(
-            activities
-        ),
-    )
-    await worker.start()
+    # Setup the workflow
+    await application.setup_workflow()
 
-    # setup application server. serves the UI, and handles the various triggers
-    app = Application(
-        handler=SQLHandler(sql_client=SQLClient()),
-        workflow_client=workflow_client,
-    )
+    # Start the worker
+    await application.start_worker()
 
-    # register the workflow on the application server
-    # the workflow is by default triggered by an HTTP POST request to the /start endpoint
-    app.register_workflow(
-        workflow_class=BaseSQLMetadataExtractionWorkflow,
-        triggers=[HttpWorkflowTrigger()],
-    )
-    await app.start()
+    # Setup the application server
+    await application.setup_server()
+
+    # Start the application server
+    await application.start_server()
 
 
 if __name__ == "__main__":
